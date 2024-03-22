24dc7d65e933d4df5eb0ca24ae03ca246afeecad
Marco Primi <marco@mpri.me>
2024-03-22 10:22:15 -0700

    [TEMP / DO NOT MERGE] Rename project to avoid name clash
    To allow release `nats` and this preview to coexhist on the same machine
    without conflict, temporarily rename this project to `nats-pa`
    
    This commit is not intended for merge and should be discarded later.

--- cli/pa_gather_command.go
+++ cli/pa_gather_command.go
@@ -25,7 +25,7 @@ import (
 	"time"
 
 	"github.com/choria-io/fisk"
-	"github.com/nats-io/natscli/archive"
+	"github.com/mprimi/natscli/archive"
 	"github.com/nats-io/nats-server/v2/server"
 	"github.com/nats-io/nats.go"
 )
